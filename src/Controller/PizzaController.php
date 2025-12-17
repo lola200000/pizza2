d542263a29b97a4a0f2e0d62e4f5e6ad4d339ece
lola200000 <mirzoyanlola@gmail.com>
2025-12-17 22:41:58 +0100

    test

---
+++ src/Controller/PizzaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PizzaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PizzaController extends AbstractController
+{
+    #[Route('/pizza', name: 'pizza_index')]
+    public function index(PizzaRepository $pizzaRepository): Response
+    {
+        return $this->render('pizza/index.html.twig', [
+            'pizzas' => $pizzaRepository->findAll(),
+        ]);
+    }
+}
---
+++ src/DataFixtures/PizzaFixtures.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Pizza;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class PizzaFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $pizzas = [
+            [
+                'name' => 'Margherita',
+                'description' => 'Tomate, mozzarella, basilic frais',
+                'price' => 10.99,
+                'image' => 'images/pizzas/margherita.jpg',
+            ],
+            [
+                'name' => 'Pepperoni',
+                'description' => 'Tomate, mozzarella, pepperoni épicé',
+                'price' => 12.99,
+                'image' => 'images/pizzas/pepperoni.jpg',
+            ],
+            [
+                'name' => 'Végétarienne',
+                'description' => 'Tomate, mozzarella, légumes frais',
+                'price' => 11.99,
+                'image' => 'images/pizzas/vegetarienne.jpg',
+            ],
+            [
+                'name' => 'Quattro Formaggi',
+                'description' => 'Mozzarella, gorgonzola, parmesan, chèvre',
+                'price' => 13.99,
+                'image' => 'images/pizzas/quattro-formaggi.jpg',
+            ],
+            [
+                'name' => 'Hawaïenne',
+                'description' => 'Tomate, mozzarella, jambon, ananas',
+                'price' => 12.49,
+                'image' => 'images/pizzas/hawaiienne.jpg',
+            ],
+            [
+                'name' => 'Calzone',
+                'description' => 'Pizza pliée garnie de jambon et ricotta',
+                'price' => 13.49,
+                'image' => 'images/pizzas/calzone.jpg',
+            ],
+        ];
+
+        foreach ($pizzas as $data) {
+            $pizza = new Pizza();
+            $pizza->setName($data['name']);
+            $pizza->setDescription($data['description']);
+            $pizza->setPrice($data['price']);
+            $pizza->setImage($data['image']);
+
+            $manager->persist($pizza);
+        }
+
+        $manager->flush();
+    }
+}
